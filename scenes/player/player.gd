acb8f869db0a97e745228a94a4ae14229a84fb46
rinevard <rinevard@outlook.com>
2025-04-05 16:28:58 +0800

    player rotate

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -12,21 +12,28 @@ extends CharacterBody2D
 @export var remove_1: PackedScene
 @export var remove_2: PackedScene
 @export var remove_3: PackedScene
-var removable: bool = false
+
+var is_rotated: bool = false
+@onready var marker_2d = $Marker2D
+
+var changable: bool = false # remove or rotate
+var cooldown_to_change: float = 0.7 # after this time, we can rotate or remove line
 
 func _input(event):
 	if event.is_action_released("tmp"):
 		remove_line(0)
 		print(height)
+	elif event.is_action_released("rotate"):
+		rotate_player()
 
 func _ready():
-	await get_tree().create_timer(1.0).timeout
-	removable = true	
+	await get_tree().create_timer(cooldown_to_change).timeout
+	changable = true	
 
 func remove_line(line: int) -> void:
-	if not removable:
+	if not changable:
 		return
-	if height == 1:
+	if height == 1 or is_rotated:
 		call_deferred("queue_free")
 		return
 	
@@ -41,11 +48,21 @@ func remove_line(line: int) -> void:
 	var rest_player: Player = remove_0.instantiate()
 	get_parent().add_child(rest_player)
 	rest_player.global_position = head_global_pos
-	print("old pos: ", global_position)
-	print("new pos: ", rest_player.global_position)
 	rest_player.velocity = velocity
 
 	call_deferred("queue_free")
+
+func rotate_player() -> void:
+	if not changable:
+		return
+	is_rotated = !is_rotated
+	
+	var center = marker_2d.global_position
+	var offset = global_position - center
+	var target_angle = deg_to_rad(90) if is_rotated else 0	
+	offset = offset.rotated(target_angle - rotation)
+	global_position = center + offset
+	rotation = target_angle
 
 func _physics_process(delta):
 	# Add the gravity.
