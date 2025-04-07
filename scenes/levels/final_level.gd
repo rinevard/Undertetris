49aa36ab175123c218305ff57aacf747972592b2
rinevard <rinevard@outlook.com>
2025-04-07 08:06:20 +0800

    polish

--- scenes/levels/final_level.gd
+++ scenes/levels/final_level.gd
@@ -1,5 +1,6 @@
 extends Level
-# level2
+# final-level
+@onready var label_score_num = $Labels/LabelScoreNum
 
 const PLAYER_I_4 = preload("res://scenes/player/player_i_4.tscn")
 var player_exists: bool = false
@@ -16,10 +17,19 @@ func _on_exit_area_body_entered(body):
 	start_next_level.emit(self, next_level)
 	print("next")
 
+var score: int = 0
+var score_arr: Array = ["999979", "999980", "999981", "999982", "999983", "999984", 
+						"999985", "999986", "999987", "999988", "999989", "999990", 
+						"999991", "999992", "999993", "999994", "999995", "999996", 
+						"999997", "999998", "999999"]
+
 func check_tiles():
 	update_movable_tetris_tiles()
 	# 从下往上遍历每一行
 	for grid_y in range(grid_down, grid_up - 1, -1):
 		if check_line(grid_y):
+			score += 1
 			RemoveSfxPlayer.play_remove()
+			score = min(score, score_arr.size() - 1)
+			label_score_num.text = score_arr[score]
 			return
